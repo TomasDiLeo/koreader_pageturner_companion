b7ae88402d5e6407cf798a67f489c738d159fb94
Tomas Di Leo <tomasjdle@gmail.com>
2026-01-12 17:22:18 -0300

    unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:io';
 import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_svg/flutter_svg.dart';
